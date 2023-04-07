64e74f1aea983687d8733a3199cbf033a529fda1
Akir Oussama <akir.oussama@gmail.com>
2023-04-07 22:29:39 +0100

    rollback i18n

--- next.config.mjs
+++ next.config.mjs
@@ -13,10 +13,10 @@ const config = {
    * must comment the below `i18n` config out.
    *
    * @see https://github.com/vercel/next.js/issues/41980
-   
+   */
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
-  },*/
+  },
 };
 export default config;
